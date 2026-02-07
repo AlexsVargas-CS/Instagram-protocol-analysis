590e588b0b4814d47cc5c237489c8f277612a296
Alexsvargas-CS <alexsvargascs@gmail.com>
2026-02-06 19:29:29 -0800

    added login authentication and a loadSession

---
+++ packages/backend/src/instagram.ts
@@ -0,0 +1,48 @@
+import { IgApiClient } from 'instagram-private-api';
+import * as fs from 'fs/promises';
+import { User, Thread, Message } from './types';
+
+export class InstagramClient {
+  private ig: IgApiClient;
+  private sessionPath: string;
+
+	constructor(sessionPath = './session.json') {
+		this.ig = new IgApiClient();
+		this.sessionPath = sessionPath;
+	}
+	
+	async login(username:string, password: string): Promise<User> {
+		
+	
+		this.ig.state.generateDevice(username); 
+		
+		await.this.ig.simulate.preLoginFlow();
+		
+		
+		const loggedInUser = await this.ig.account.login(username, password); //login fails catch will be added later...
+		
+		await this.saveSession(); //IG call expensive so save good 
+		
+		return this.mapUser(loggedInUser);
+	}
+	
+	async loadSession(): Promise<boolean> {//session is either restored or not.
+		//get the session/ read it, feed saved state back into the library's internal state machine, call user to make sure session hasnt expired.
+		try {
+			data = await fs.readFile(this.sessionPath, 'utf-8'); //get session and store
+			const stateObject = JSON.parse(data);
+			await this.ig.state.deserialize(stateObject); //restores session state from a saved JSON string 
+			
+			//quickly validiating user session, making sure its not expired
+			await this.ig.account.currentUser();
+			return true;
+		}catch {
+			return false;
+		}
+		
+	}
+	
+	
+	
+
+}
